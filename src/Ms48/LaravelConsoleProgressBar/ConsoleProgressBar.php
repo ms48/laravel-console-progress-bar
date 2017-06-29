f9c5a5ba7ec16d1b9b5954ea5dcc7957bf7407a3
Shanuka <shanuka@asiamediaglobal.com>
2017-06-29 10:56:31 +0530

    code improvements according to PSR2

--- src/Ms48/LaravelConsoleProgressBar/ConsoleProgressBar.php
+++ src/Ms48/LaravelConsoleProgressBar/ConsoleProgressBar.php
@@ -16,21 +16,21 @@ class ConsoleProgressBar
      *
      * @var int
      */
-    private $currentCount;
+    protected $currentCount;
     
     /**
      * Max length of the line.
      *
      * @var int
      */
-    private $maxLength;
+    protected $maxLength;
     
     /**
      * Progress start time.
      *
      * @var \Datetime
      */
-    private $startTime;
+    protected $startTime;
 
     /**
      * Create a new console Progress Bar.
@@ -45,7 +45,7 @@ class ConsoleProgressBar
     /**
      * Reset the values to default;
      */
-    private function resetValues()
+    protected function resetValues()
     {
         $this->currentCount=0;
         $this->maxLength=0;
@@ -58,7 +58,7 @@ class ConsoleProgressBar
      * @param   string  $seconds   how many seconds you want to convert
      * @return  string
      */
-    private function convertSecondsToHMS($seconds) 
+    protected function convertSecondsToHMS($seconds) 
     {
          // extract hours
         $hours = (floor($seconds / (60 * 60))>0)?floor($seconds / (60 * 60))."h ":"";
@@ -76,13 +76,13 @@ class ConsoleProgressBar
     }
     
     /**
-     * When current line length lesser than the previous one, we should fill the
-     * remaining characters with spaces.
+     * When current line length is lesser than to the previous one, 
+     * we should fill the remaining characters with spaces.
      *
      * @param   string text  Status bar text
      * @return  string     
      */
-    private function maintainLength($text)
+    protected function maintainLength($text)
     {
         $strLength = strlen($text);
         if($this->maxLength > $strLength){
@@ -95,14 +95,31 @@ class ConsoleProgressBar
     }
     
     /**
-     * Generate status bar   
+     * Generate progress bar   
+     *
+     * @param   int     $total   how many items are to be done total
+     * @param   int     $size  size of the status bar
+     * @param   int     $elapsed   elapsed time in sec
+     * @return  string     
+     */
+    protected function showCompletedBar($total, $size, $elapsed)
+    {
+        $statusBar = "\r[".str_repeat("=", $size+1) 
+                     . "] 100% $total/$total remaining: 0 sec. "
+                     . "elapsed: " . $this->convertSecondsToHMS($elapsed) . "\n";
+        
+        return $this->maintainLength($statusBar);
+    }        
+    
+    /**
+     * Generate progress bar   
      *
      * @param   int     $done   how many items are completed
      * @param   int     $total  how many items are to be done total
      * @param   int     $size   optional size of the status bar
      * @return  void     
      */
-    private function showStatus($done, $total, $size = 30){
+    protected function generateProgress($done, $total, $size = 30){
         //if empty start time, get currunt time as start time
         if (empty($this->startTime)){
             $this->startTime = time();
@@ -113,16 +130,18 @@ class ConsoleProgressBar
 
         // if we go over our bound or finished the process, display 100% and just ignore it
         if ($done >= $total){
-            $statusBar = "\r[".str_repeat("=", $size+1) ."] 100% $total/$total remaining: 0 sec. elapsed: " .
-                                            $this->convertSecondsToHMS($elapsed) . "\n";
-            echo $this->maintainLength($statusBar);
+            echo $this->showCompletedBar($total, $size, $elapsed);
             $this->resetValues();
             return;
         }  
-
+        
+        //calculate the precentage
         $perc = (double) ($done / $total);
         $bar = floor($perc * $size);
+        
+        $disp = number_format($perc * 100, 0);
 
+        //make the status bar
         $statusBar = "\r[";
         $statusBar .= str_repeat("=", $bar);
         if ($bar < $size) {
@@ -132,10 +151,9 @@ class ConsoleProgressBar
             $statusBar .= "=";
         }
 
-        $disp = number_format($perc * 100, 0);
-
         $statusBar .= "] $disp%  $done/$total";
 
+        //calculate the ETA
         $rate = ($now - $this->startTime) / $done;
         $left = $total - $done;
         $eta = round($rate * $left);
@@ -146,7 +164,8 @@ class ConsoleProgressBar
     }
     
     /**
-     * Show a progress with limit in the console
+     * Generate a current progress output. We should take care of the looping
+     * trough the collection.
      *
      * @param   int     $limit  data block size
      * @param   int     $total  how many items are to be done total
@@ -155,6 +174,6 @@ class ConsoleProgressBar
      */
     public function showProgress($limit, $total, $size = 30){
         $this->currentCount += $limit;
-        $this->showStatus($this->currentCount, $total, $size);
+        $this->generateProgress($this->currentCount, $total, $size);
     }
 }
